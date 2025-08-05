240927e4d3fd297be6c84050c67c4718597f193e
HarshJB5 <harsadeepta2025@gmail.com>
2025-08-05 16:21:54 +0530

    Add files via upload

---
+++ Task 1.py
@@ -0,0 +1,13 @@
+a=input('Enter a number:')
+a=float(a)
+b=input('Enter another number:')
+b=float(b)
+
+p=a+b
+q=a-b
+r=a*b
+x=a/b
+print("Addition:",p)
+print("Subtraction:",q)
+print("Multiplication:",r)
+print("Division:",x)
---
+++ Task 2.py
@@ -0,0 +1,6 @@
+#custom invitation
+a=input("Enter your First Name")
+b=input("Enter your Last Name")
+c=a+" "+b
+d= "Hello, "+c+"!"+" Welcome to the Python Program."
+print(d)
